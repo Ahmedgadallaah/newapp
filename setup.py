4d7b7e8ec0b5314d94e7bb0ecf1ee2e611309509
ahmedgadallah <43891136+Ahmedgadallaah@users.noreply.github.com>
2024-08-01 14:25:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
         install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in zatca2024/__init__.py
-from newapp import __version__ as version
-
 setup(
         name='net_contract',
         version=version,
