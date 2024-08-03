383efd066ada333c1bd6d0340fd828b677194d1e
ahmedgadallah <43891136+Ahmedgadallaah@users.noreply.github.com>
2024-08-03 16:55:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+# # -*- coding: utf-8 -*-
+# from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-        install_requires = f.read().strip().split('\n')
+# with open('requirements.txt') as f:
+#         install_requires = f.read().strip().split('\n')
 
-setup(
-        name='net_contract',
-        version='',
-        description='net_contract',
-        author='Ahmed',
-        author_email='info@qp.sa',
-        packages=find_packages(),
-        zip_safe=False,
-        include_package_data=True,
-        install_requires=install_requires
-)
+# setup(
+#         name='net_contract',
+#         version='',
+#         description='net_contract',
+#         author='Ahmed',
+#         author_email='info@qp.sa',
+#         packages=find_packages(),
+#         zip_safe=False,
+#         include_package_data=True,
+#         install_requires=install_requires
+# )
 
